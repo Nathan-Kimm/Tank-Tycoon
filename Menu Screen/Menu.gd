431921a3cec54f0a0320211eb1554ddcb59f7d73
Matthew Seo <matt_seo@yahoo.com>
2024-06-02 19:08:18 -0400

    Music Slider fix and added SFX

--- Menu Screen/Menu.gd
+++ Menu Screen/Menu.gd
@@ -11,12 +11,28 @@ func _ready():
 	settings.hide()
 
 func _on_button_pressed():
+	$"press sound".play()
 	scene_transition_animation.play("fade_in")
 	#await get_tree().create_timer(0.5).timeout
 	get_tree().change_scene_to_file("res://Main Tank/tank.tscn")
 
 func _on_setting_pressed():
+	$"press sound".play()
 	settings.show()
 
 func _on_quit_pressed():
+	$"press sound".play()
 	get_tree().quit()
+
+
+func _on_play_mouse_entered():
+	$"Hover sound".play()
+	pass # Replace with function body.
+func _on_quit_mouse_entered():
+	$"Hover sound".play()
+	pass # Replace with function body.
+func _on_setting_mouse_entered():
+	$"Hover sound".play()
+	pass # Replace with function body.
+	
+	
